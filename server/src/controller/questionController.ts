fb148c78319ace4becb2edb6cdba0298c4ff7cf3
David <davidsansan@usal.es>
2023-05-25 01:18:52 +0200

    .

--- server/src/controller/questionController.ts
+++ server/src/controller/questionController.ts
@@ -2,13 +2,10 @@
 
 import * as Express from "express";
 
-import { QuestionModel } from "../model/questionModel";
 import { STATUS_OK, STATUS_BAD_REQUEST, CONTENT_APPLICATION_JSON, STATUS_INTERNAL_SERVER_ERROR } from "../utils";
 import { AuthMiddleware } from "../auth";
 
 export class QuestionController {
-
-    public static questionModel: QuestionModel = new QuestionModel();
 
     /**
      * Register all the routes for the user controller
@@ -18,11 +15,23 @@ export class QuestionController {
 
     public registerController(app: Express.Express, path: string): void {
         app.get(path + '/', AuthMiddleware.checkToken, this.getQuestions.bind(this));
+        app.post(path + '/', AuthMiddleware.checkToken, this.postAnswer.bind(this));
     }
 
-    public getQuestions(request: any, response: Express.Response): void {
+    public async getQuestions(request: any, response: Express.Response) {
 
-        QuestionController.questionModel.getQuestions();
+        const url = 'https://opentdb.com/api.php?amount=10';
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            //For question in data.results data.results[0]['time'] = Date.now();
+            data.results[0]['timeStart'] = Date.now();
+            request.session.questions = data.results;
+        } catch (error) {
+            console.log(error);
+        }
+        
         response.status(STATUS_OK);
         response.contentType(CONTENT_APPLICATION_JSON);
         response.json({ "code": STATUS_OK, "message": "Questions retrieved" });
@@ -30,4 +39,27 @@ export class QuestionController {
     
     }
 
+    public postAnswer(request: any, response: Express.Response): void {
+
+        //Id and answer are sent in the body of the request
+        const id: number = request.body.id || null;
+        const answer: string = request.body.answer || null;
+
+        if (!id || !answer) {
+            response.status(STATUS_BAD_REQUEST);
+            response.contentType(CONTENT_APPLICATION_JSON);
+            response.json({ "code": STATUS_BAD_REQUEST, "message": "Missing parameters" });
+        }
+
+        //Save the answer in the question
+        request.session.questions[id]['answer'] = answer;
+        request.session.questions[id]['timeEnd'] = Date.now();
+
+        //If id < 9, startTime of the next question is the endTime of the current question
+        if (id < 9) {
+            request.session.questions[id + 1]['timeStart'] = Date.now();
+        } else {
+            console.log(request.session.questions);
+        }
+    }
 }
